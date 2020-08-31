c0cd5a872f21efda12c8e6972f9ed1c798b03f60
ioedumac <eacevedof@yahoo.es>
2020-08-31 18:52:54 +0200

    insert blade

--- backend_web/resources/views/restrict/post/insert.blade.php
+++ backend_web/resources/views/restrict/post/insert.blade.php
@@ -2,8 +2,10 @@
 
 @section('content')
 <div class="container">
-    <h1>New article</h1>
     <div class="card">
+        <div class="card-header">
+            <h1>New article</h1>
+        </div>
         <div class="card-body">
             <form action="">
                 <div class="form-group">
