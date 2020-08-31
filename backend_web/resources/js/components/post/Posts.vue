57226b19158eca90e1802ee966f8c2765128032d
ioedumac <eacevedof@yahoo.es>
2020-08-31 21:08:46 +0200

    ya se ve posts component

---
+++ backend_web/resources/js/components/post/Posts.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+    Post List Component
+</div>
+</template>
+
+<script>
+export default {
+    name: "posts",
+    mounted() {
+        console.log('Post List mounted.')
+    }
+}
+</script>
