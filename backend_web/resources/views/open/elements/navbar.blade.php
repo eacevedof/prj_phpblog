c65ac2bb6cd101aedd1d98cd76dde3ebeac2ea01
ioedumac <eacevedof@yahoo.es>
2020-08-31 15:05:38 +0200

    configuro /blog

--- backend_web/resources/views/open/elements/navbar.blade.php
+++ backend_web/resources/views/open/elements/navbar.blade.php
@@ -1,30 +1,32 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
-    <a class="navbar-brand" href="{{ route('open.home') }}">Eduardo A. F.</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{ route('open.home') }}">Eduardo A. F.</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarsExample09">
+            <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('open.home.aboutme') }}">Sobre mí</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('open.home.contact') }}">Contacto</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                        Blog
+                    </a>
 
-    <div class="collapse navbar-collapse" id="navbarsExample09">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('open.home.aboutme') }}">Sobre mí</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('open.home.contact') }}">Contacto</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle"
-                   href="{{ route('open.home.contact') }}" id="dropdown09" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown09">
-                    <a class="dropdown-item" href="/blog/php/">php</a>
-                    <a class="dropdown-item" href="/blog/js">js</a>
-                    <a class="dropdown-item" href="/blog/sql">sql</a>
-                    <a class="dropdown-item" href="/blog/python">python</a>
-                </div>
-            </li>
-        </ul>
-        <form class="form-inline my-2 my-md-0">
-            <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-        </form>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="/blog/php/">php</a>
+                        <a class="dropdown-item" href="/blog/js">js</a>
+                        <a class="dropdown-item" href="/blog/sql">sql</a>
+                        <a class="dropdown-item" href="/blog/python">python</a>
+                    </ul>
+                </li>
+
+            </ul>
+            @include("open/forms/form-search")
+        </div>
     </div>
 </nav>
