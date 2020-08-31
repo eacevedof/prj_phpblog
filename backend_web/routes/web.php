e214d95c6dad2f7cf1cf8b96d9cb7272eed0f4db
ioedumac <eacevedof@yahoo.es>
2020-08-31 15:43:42 +0200

    despues de instalar el autologin y register haciendo refactor con basecontroller

--- backend_web/routes/web.php
+++ backend_web/routes/web.php
@@ -20,3 +20,7 @@ Route::get('/eduardo-acevedo-farje', ['as' => 'open.home.aboutme', 'uses' => 'Op
 Route::get('/blog/{slug}', ['as' => 'open.blog.category', 'uses' => 'Open\BlogController@category']);
 Route::get('/blog', ['as' => 'open.blog', 'uses' => 'Open\BlogController']);
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
