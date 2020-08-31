a5b5d167e9421b0a3cbbae33d92aa822d21dd83d
ioedumac <eacevedof@yahoo.es>
2020-08-31 12:35:47 +0200

    refactor Trait Log

--- backend_web/app/Http/Controllers/BaseController.php
+++ backend_web/app/Http/Controllers/BaseController.php
@@ -7,7 +7,11 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as RoutingController;
 
+use App\Traits\Log;
+
 class BaseController extends RoutingController
 {
+    use Log;
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
 }
--- backend_web/app/Models/BaseModel.php
+++ backend_web/app/Models/BaseModel.php
@@ -3,8 +3,9 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Log;
 
 class BaseModel extends Model
 {
-
+    use Log;
 }
