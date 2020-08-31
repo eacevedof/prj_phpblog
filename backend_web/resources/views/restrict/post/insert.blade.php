04a1283072f9c7efcfcc804a6936a7067c232b4f
ioedumac <eacevedof@yahoo.es>
2020-08-31 19:35:43 +0200

    mejoro insert

--- backend_web/resources/views/restrict/post/insert.blade.php
+++ backend_web/resources/views/restrict/post/insert.blade.php
@@ -10,23 +10,11 @@
             <form action="" method="post">
                 @csrf
                 <div class="form-row">
-                    <div class="form-group col-md-6">
+                    <div class="form-group col-md-4">
                         <label for="txt-description">Hidden description</label>
                         <input type="text" id="txt-description" name="description" maxlength="250" class="form-control"/>
                     </div>
-                    <div class="form-group col-md-6">
-                        <label for="txt-slug">Slug</label>
-                        <input type="text" id="txt-slug" name="slug" maxlength="150" class="form-control"/>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="txt-url_final">Permalink</label>
-                        <input type="text" id="chk-url_final" name="url_final" maxlength="300" class="form-control"/>
-                    </div>
-                    <div class="form-check col-md-6" style="padding-top:35px">
-                        <input type="checkbox" id="chk-is_page" name="is_page" value="1" />
-                        <label for="chk-is_page" class="form-check-label"><b>Is single page</b></label>
-                    </div>
-                    <div class="form-group col-md-6">
+                    <div class="form-group col-md-4">
                         <label for="sel-id_type">Category</label>
                         <select id="sel-id_type" name="id_type" class="form-control">
                             <option value="">Choose one</option>
@@ -36,7 +24,34 @@
                             <option value="blog-docker">Docker</option>
                         </select>
                     </div>
-
+                    <div class="form-check col-md-4" style="padding-top:35px">
+                        <input type="checkbox" id="chk-is_page" name="is_page" value="1" />
+                        <label for="chk-is_page" class="form-check-label"><b>Is single page</b></label>
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label for="txt-slug">Slug</label>
+                        <input type="text" id="txt-slug" name="slug" maxlength="150" class="form-control"/>
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label for="txt-url_final">Permalink</label>
+                        <input type="text" id="chk-url_final" name="url_final" maxlength="300" class="form-control"/>
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label for="txt-title">title</label>
+                        <input type="text" id="txt-title" name="title" maxlength="350" class="form-control">
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label for="txt-subtitle">subtitle</label>
+                        <input type="text" id="txt-subtitle" name="subtitle" maxlength="250" class="form-control">
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label for="txa-content">content</label>
+                        <textarea id="txa-content" name="content" rows="10" cols="10" class="form-control"></textarea>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="txa-excerpt">excerpt</label>
+                        <textarea id="txa-excerpt" name="excerpt" maxlength="1000" rows="4" cols="5" class="form-control"></textarea>
+                    </div>
                     <div class="form-group col-md-6">
                         <label for="txt-url_img1">Url img1</label>
                         <input type="text" id="chk-url_img1" name="url_img1" maxlength="300" class="form-control"/>
@@ -49,40 +64,24 @@
                         <label for="txt-url_img3">Url img3</label>
                         <input type="text" id="chk-url_img3" name="url_img3" maxlength="300" class="form-control">
                     </div>
-                    <div class="form-group col-md-6">
-                        <label for="txt-title">title</label>
-                        <input type="text" id="txt-title" name="title" maxlength="350" class="form-control">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="txt-subtitle">subtitle</label>
-                        <input type="text" id="txt-subtitle" name="subtitle" maxlength="250" class="form-control">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="txa-excerpt">excerpt</label>
-                        <textarea id="txa-excerpt" name="excerpt" maxlength="1000" rows="4" cols="5" class="form-control"></textarea>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="txa-content">content</label>
-                        <textarea id="txa-content" name="content" rows="10" cols="10" class="form-control"></textarea>
-                    </div>
-                    <div class="form-group col-md-6">
+                    <div class="form-group col-md-3">
                         <label for="sel-id_user">User</label>
                         <select id="sel-id_user" name="id_user" class="form-control">
                             <option value="1">1</option>
                         </select>
                     </div>
-                    <div class="form-group col-md-6">
+                    <div class="form-group col-md-3">
                         <label for="sel-id_status">Status</label>
                         <select id="sel-id_status" name="id_status" class="form-control">
                             <option value="0">Disabled</option>
                             <option value="1">Published</option>
                         </select>
                     </div>
-                    <div class="form-group col-md-6">
+                    <div class="form-group col-md-3">
                         <label for="dat-publish_date">Published</label>
                         <input type="date" id="dat-publish_date" name="publish_date" class="form-control" />
                     </div>
-                    <div class="form-group col-md-6">
+                    <div class="form-group col-md-3">
                         <label for="dat-last_update">Last update</label>
                         <input type="date" id="dat-last_update" name="last_update" class="form-control" />
                     </div>
