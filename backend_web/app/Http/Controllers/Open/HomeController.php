cbdb11bddc8e0b228d363ab4c760cc77aeb3fc61
ioedumac <eacevedof@yahoo.es>
2020-08-31 12:32:17 +0200

    refactor con BaseXXX

--- backend_web/app/Http/Controllers/Open/HomeController.php
+++ backend_web/app/Http/Controllers/Open/HomeController.php
@@ -1,9 +1,9 @@
 <?php
 namespace App\Http\Controllers\Open;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\BaseController;
 
-class HomeController extends Controller
+class HomeController extends BaseController
 {
     /**
      * @return View
