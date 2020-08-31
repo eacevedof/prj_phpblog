5ee3810349f16c597b8dfc5670ff3b0fd37fb5ca
ioedumac <eacevedof@yahoo.es>
2020-08-31 22:28:18 +0200

    ya pinto tabla

--- backend_web/resources/js/components/post/Posts.vue
+++ backend_web/resources/js/components/post/Posts.vue
@@ -4,13 +4,42 @@
         <h1>Articles</h1>
     </div>
     <div class="card-body">
-
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>2018</th>
+                <th>2017</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, index) in rows" :key="index">
+                    <td v-for="(column, indexColumn) in columns" :key="indexColumn">{{item.$column}}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return {
+            columns: ["master","detail"],
+            rows: [
+                {
+                    master: "Master",
+                    detail: "Detail"
+                },
+                {
+                    master: "Master",
+                    detail: "Detail"
+                },
+            ],
+        }
+    },
+
     mounted() {
         console.log('Post List mounted.')
     }
