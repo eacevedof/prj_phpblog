5e9402e3cdc809130545f8c562b8128cb865f788
ioedumac <eacevedof@yahoo.es>
2020-09-02 15:44:51 +0200

    cambio de form a post y creo UsersTableSeeder

--- backend_web/resources/js/components/restrict/post/PostInsert.vue
+++ backend_web/resources/js/components/restrict/post/PostInsert.vue
@@ -8,11 +8,11 @@
                 <div class="form-row">
                     <div class="form-group col-md-4">
                         <label for="txt-description">Hidden description</label>
-                        <input type="text" id="txt-description" v-model="form.description" maxlength="250" class="form-control"/>
+                        <input type="text" id="txt-description" v-model="post.description" maxlength="250" class="form-control"/>
                     </div>
                     <div class="form-group col-md-4">
                         <label for="sel-id_type">Category</label>
-                        <select id="sel-id_type" v-model="form.id_type" class="form-control">
+                        <select id="sel-id_type" v-model="post.id_type" class="form-control">
                             <option value="">Choose one</option>
                             <option value="single-page">Single page</option>
                             <option value="blog-php">Php</option>
@@ -21,79 +21,79 @@
                         </select>
                     </div>
                     <div class="form-check col-md-4" style="padding-top:35px">
-                        <input type="checkbox" id="chk-is_page" v-model="form.is_page" value="1" />
+                        <input type="checkbox" id="chk-is_page" v-model="post.is_page" value="1" />
                         <label for="chk-is_page" class="form-check-label"><b>Is single page</b></label>
                     </div>
                     <div class="form-group col-md-12">
                         <label for="txt-slug">Slug</label>
-                        <input type="text" id="txt-slug" v-model="form.slug" maxlength="150" class="form-control"/>
+                        <input type="text" id="txt-slug" v-model="post.slug" maxlength="150" class="form-control"/>
                     </div>
                     <div class="form-group col-md-12">
                         <label for="txt-url_final">Permalink</label>
-                        <input type="text" id="txt-url_final" v-model="form.url_final" maxlength="300" class="form-control"/>
+                        <input type="text" id="txt-url_final" v-model="post.url_final" maxlength="300" class="form-control"/>
                     </div>
                     <div class="form-group col-md-12">
                         <label for="txt-title">title</label>
-                        <input type="text" id="txt-title" v-model="form.title" maxlength="350" class="form-control">
+                        <input type="text" id="txt-title" v-model="post.title" maxlength="350" class="form-control">
                     </div>
                     <div class="form-group col-md-12">
                         <label for="txt-subtitle">subtitle</label>
-                        <input type="text" id="txt-subtitle" v-model="form.subtitle" maxlength="250" class="form-control">
+                        <input type="text" id="txt-subtitle" v-model="post.subtitle" maxlength="250" class="form-control">
                     </div>
                     <div class="form-group col-md-12">
                         <label for="txa-content">content</label>
-                        <textarea id="txa-content" v-model="form.content" rows="15" cols="10" class="form-control"></textarea>
+                        <textarea id="txa-content" v-model="post.content" rows="15" cols="10" class="form-control"></textarea>
                     </div>
                     <div class="form-group col-md-12">
                         <label for="txa-excerpt">excerpt</label>
-                        <textarea id="txa-excerpt" v-model="form.excerpt" maxlength="1000" rows="3" cols="5" class="form-control"></textarea>
+                        <textarea id="txa-excerpt" v-model="post.excerpt" maxlength="1000" rows="3" cols="5" class="form-control"></textarea>
                     </div>
 
                     <div class="form-group col-md-12">
                         <label for="txt-url_img1">Url img1</label>
-                        <input type="text" id="txt-url_img1" v-model="form.url_img1" maxlength="300" class="form-control"/>
+                        <input type="text" id="txt-url_img1" v-model="post.url_img1" maxlength="300" class="form-control"/>
                     </div>
                     <div class="form-group col-md-6">
                         <label for="txt-url_img2">Url img2</label>
-                        <input type="text" id="txt-url_img2" v-model="form.url_img2" maxlength="300" class="form-control"/>
+                        <input type="text" id="txt-url_img2" v-model="post.url_img2" maxlength="300" class="form-control"/>
                     </div>
                     <div class="form-group col-md-6">
                         <label for="txt-url_img3">Url img3</label>
-                        <input type="text" id="txt-url_img3" v-model="form.url_img3" maxlength="300" class="form-control">
+                        <input type="text" id="txt-url_img3" v-model="post.url_img3" maxlength="300" class="form-control">
                     </div>
 
                     <div class="form-group col-md-3">
                         <label for="sel-id_user">User</label>
-                        <select id="sel-id_user" v-model="form.id_user" class="form-control">
+                        <select id="sel-id_user" v-model="post.id_user" class="form-control">
                             <option value="1">1</option>
                         </select>
                     </div>
                     <div class="form-group col-md-3">
                         <label for="sel-id_status">Status</label>
-                        <select id="sel-id_status" v-model="form.id_status" class="form-control">
+                        <select id="sel-id_status" v-model="post.id_status" class="form-control">
                             <option value="0">Disabled</option>
                             <option value="1">Published</option>
                         </select>
                     </div>
                     <div class="form-group col-md-3">
                         <label for="dat-publish_date">Published</label>
-                        <input type="date" id="dat-publish_date" v-model="form.publish_date" class="form-control" />
+                        <input type="date" id="dat-publish_date" v-model="post.publish_date" class="form-control" />
                     </div>
                     <div class="form-group col-md-3">
                         <label for="dat-last_update">Last update</label>
-                        <input type="date" id="dat-last_update" v-model="form.last_update" class="form-control" />
+                        <input type="date" id="dat-last_update" v-model="post.last_update" class="form-control" />
                     </div>
                     <div class="form-group col-md-6">
                         <label for="txt-seo_title">SEO Title</label>
-                        <input type="text" id="txt-seo_title" v-model="form.seo_title" maxlength="65" class="form-control"/>
+                        <input type="text" id="txt-seo_title" v-model="post.seo_title" maxlength="65" class="form-control"/>
                     </div>
                     <div class="form-group col-md-6">
                         <label for="txt-seo_description">SEO Description</label>
-                        <input type="text" id="txt-seo_description" v-model="form.seo_description" maxlength="160" class="form-control"/>
+                        <input type="text" id="txt-seo_description" v-model="post.seo_description" maxlength="160" class="form-control"/>
                     </div>
                     <div class="form-group col-md-2">
                         <label for="num-order_by">Position</label>
-                        <input type="number" id="num-order_by" v-model="form.order_by" value="100" class="form-control"/>
+                        <input type="number" id="num-order_by" v-model="post.order_by" value="100" class="form-control"/>
                     </div>
                     <div class="form-group col-md-4">
                         <button class="btn btn-primary" :disabled="issending" style="margin-top:28px;" >
@@ -109,7 +109,7 @@
 <script>
 import custom from "../../../custom"
 let csrftoken = custom.get_csrftoken()
-console.log(csrftoken,"csrftoken")
+//console.log(csrftoken,"csrftoken")
 const BTN_INISTATE = "Guardar"
 const BTN_IN_PROGRESS = "Procesando..."
 const BTN_CONFIRM = "Confirmar"
@@ -119,7 +119,7 @@ export default {
         return {
             btnsend: BTN_INISTATE,
             issending: false,
-            form: {
+            post: {
                 description: "",
                 id_type: 0,
                 is_page: 0,
@@ -148,29 +148,29 @@ export default {
             const self = this
             self.issending = true
             self.btnsend = BTN_IN_PROGRESS
-            const url = `/adm/post/insert`
-            const data = new FormData();
+            const url = `/api/post`
+            const data = new postData();
 
             data.append("_token",csrftoken)
             data.append("action","post.insert")
-            data.append("description",this.form.description)
-            data.append("id_type",this.form.id_type)
-            data.append("is_page",this.form.is_page)
-            data.append("slug",this.form.slug)
-            data.append("url_final",this.form.url_final)
-            data.append("title",this.form.title)
-            data.append("subtitle",this.form.subtitle)
-            data.append("content",this.form.content)
-            data.append("excerpt",this.form.excerpt)
-            data.append("url_img1",this.form.url_img1)
-            data.append("url_img2",this.form.url_img2)
-            data.append("url_img3",this.form.url_img3)
-            data.append("id_user",this.form.id_user)
-            data.append("publish_date",this.form.publish_date)
-            data.append("last_update",this.form.last_update)
-            data.append("seo_title",this.form.seo_title)
-            data.append("seo_description",this.form.seo_description)
-            data.append("order_by",this.form.order_by)
+            data.append("description",this.post.description)
+            data.append("id_type",this.post.id_type)
+            data.append("is_page",this.post.is_page)
+            data.append("slug",this.post.slug)
+            data.append("url_final",this.post.url_final)
+            data.append("title",this.post.title)
+            data.append("subtitle",this.post.subtitle)
+            data.append("content",this.post.content)
+            data.append("excerpt",this.post.excerpt)
+            data.append("url_img1",this.post.url_img1)
+            data.append("url_img2",this.post.url_img2)
+            data.append("url_img3",this.post.url_img3)
+            data.append("id_user",this.post.id_user)
+            data.append("publish_date",this.post.publish_date)
+            data.append("last_update",this.post.last_update)
+            data.append("seo_title",this.post.seo_title)
+            data.append("seo_description",this.post.seo_description)
+            data.append("order_by",this.post.order_by)
 
 
             fetch(url, {
@@ -179,40 +179,40 @@ export default {
                 //si voy a enviar un json
                 //headers:{'Content-Type': 'application/json'},
             })
-            //.then(response => console.log(response,"RESPONSE"))
-            .then(response => response.json())
-            .then(response => {
-                self.issending = false
-                self.btnsend = BTN_INISTATE
+                //.then(response => console.log(response,"RESPONSE"))
+                .then(response => response.json())
+                .then(response => {
+                    self.issending = false
+                    self.btnsend = BTN_INISTATE
 
-                console.log("reponse",response)
+                    console.log("reponse",response)
 
-                if(response.title == "success") {
-                    Swal.fire({
-                        icon: 'success',
-                        title: `Post: "${self.form.description}" <br/> creado`,
-                        html: `<b>&#128578;</b>`,
-                    })
-                    self.showconfirm = true;
-                    self.btnsend = BTN_CONFIRM
-                } else {
-                    Swal.fire({
-                        icon: 'warning',
-                        title: 'Esta acción no se ha podido completar',
-                        text: response.description,
-                    })
-                }
-            })
-            .catch(error => {
-                self.issending = false
-                self.btnsend = BTN_INISTATE
-                console.log("CATCH ERROR insert",error)
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Vaya! Ha ocurrido un error',
-                    text: error.toString(),
+                    if(response.title == "success") {
+                        Swal.fire({
+                            icon: 'success',
+                            title: `Post: "${self.post.description}" <br/> creado`,
+                            html: `<b>&#128578;</b>`,
+                        })
+                        self.showconfirm = true;
+                        self.btnsend = BTN_CONFIRM
+                    } else {
+                        Swal.fire({
+                            icon: 'warning',
+                            title: 'Esta acción no se ha podido completar',
+                            text: response.description,
+                        })
+                    }
                 })
-            })
+                .catch(error => {
+                    self.issending = false
+                    self.btnsend = BTN_INISTATE
+                    console.log("CATCH ERROR insert",error)
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Vaya! Ha ocurrido un error',
+                        text: error.toString(),
+                    })
+                })
         },//insert
 
         handleSubmit: function(e) {
