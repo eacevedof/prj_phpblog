fc6c32b6bd0f85fb80f7e1586174d9f713b4652a
ioedumac <eacevedof@yahoo.es>
2020-09-02 20:31:23 +0200

    PostUpdate.vue

--- backend_web/resources/js/app.js
+++ backend_web/resources/js/app.js
@@ -14,9 +14,11 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 import Posts from "./components/restrict/post/Posts.vue";
 import PostInsert from "./components/restrict/post/PostInsert.vue";
+import PostUpdate from "./components/restrict/post/PostUpdate.vue";
 
 Vue.component('posts', Posts);
 Vue.component('postinsert', PostInsert);
+Vue.component('postupdate', PostUpdate);
 
 const app = new Vue({
     el: '#app',
