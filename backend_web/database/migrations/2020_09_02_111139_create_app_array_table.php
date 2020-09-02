bd4f3990ae31d131952a3ed9f7ed84a80d1bb15f
ioedumac <eacevedof@yahoo.es>
2020-09-02 13:56:27 +0200

    fix en migrations files

--- backend_web/database/migrations/2020_09_02_111139_create_app_array_table.php
+++ backend_web/database/migrations/2020_09_02_111139_create_app_array_table.php
@@ -15,28 +15,28 @@ class CreateAppArrayTable extends Migration {
 	{
 		Schema::create('app_array', function(Blueprint $table)
 		{
-			$table->string(''processflag'', 5)->nullable();
-			$table->string(''insert_platform'', 3)->nullable()->default('1');
-			$table->string(''insert_user'', 15)->nullable();
+			$table->string('processflag', 5)->nullable();
+			$table->string('insert_platform', 3)->nullable()->default('1');
+			$table->string('insert_user', 15)->nullable();
 			$table->timestamp('insert_date')->default(DB::raw('CURRENT_TIMESTAMP'));
-			$table->string(''update_platform'', 3)->nullable();
-			$table->string(''update_user'', 15)->nullable();
+			$table->string('update_platform', 3)->nullable();
+			$table->string('update_user', 15)->nullable();
 			$table->timestamp('update_date')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
-			$table->string(''delete_platform'', 3)->nullable();
-			$table->string(''delete_user'', 15)->nullable();
+			$table->string('delete_platform', 3)->nullable();
+			$table->string('delete_user', 15)->nullable();
 			$table->dateTime('delete_date')->nullable();
-			$table->string(''cru_csvnote'', 500)->nullable();
-			$table->string(''is_erpsent'', 3)->nullable()->default('0');
-			$table->string(''is_enabled'', 3)->nullable()->default('1');
+			$table->string('cru_csvnote', 500)->nullable();
+			$table->string('is_erpsent', 3)->nullable()->default('0');
+			$table->string('is_enabled', 3)->nullable()->default('1');
 			$table->integer('i')->nullable();
-			$table->integer(''id'', true);
-			$table->string(''code_erp'', 25)->nullable();
-			$table->string(''type'', 15)->nullable();
-			$table->string(''id_tosave'', 25)->nullable();
-			$table->string(''description'', 250)->nullable();
+			$table->integer('id', true);
+			$table->string('code_erp', 25)->nullable();
+			$table->string('type', 15)->nullable();
+			$table->string('id_tosave', 25)->nullable();
+			$table->string('description', 250)->nullable();
 			$table->integer('id_user')->nullable()->comment('propietario del tipo o categoria');
 			$table->integer('order_by')->default(100);
-			$table->string(''code_cache'', 50)->nullable();
+			$table->string('code_cache', 50)->nullable();
 		});
 	}
 
--- backend_web/database/migrations/2020_09_02_111139_create_app_posts_tags_table.php
+++ backend_web/database/migrations/2020_09_02_111139_create_app_posts_tags_table.php
@@ -15,25 +15,25 @@ class CreateAppPostsTagsTable extends Migration {
 	{
 		Schema::create('app_posts_tags', function(Blueprint $table)
 		{
-			$table->string(''processflag'', 5)->nullable();
-			$table->string(''insert_platform'', 3)->nullable()->default('1');
-			$table->string(''insert_user'', 15)->nullable();
+			$table->string('processflag', 5)->nullable();
+			$table->string('insert_platform', 3)->nullable()->default('1');
+			$table->string('insert_user', 15)->nullable();
 			$table->timestamp('insert_date')->default(DB::raw('CURRENT_TIMESTAMP'));
-			$table->string(''update_platform'', 3)->nullable();
-			$table->string(''update_user'', 15)->nullable();
+			$table->string('update_platform', 3)->nullable();
+			$table->string('update_user', 15)->nullable();
 			$table->timestamp('update_date')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
-			$table->string(''delete_platform'', 3)->nullable();
-			$table->string(''delete_user'', 15)->nullable();
+			$table->string('delete_platform', 3)->nullable();
+			$table->string('delete_user', 15)->nullable();
 			$table->dateTime('delete_date')->nullable();
-			$table->string(''cru_csvnote'', 500)->nullable();
-			$table->string(''is_erpsent'', 3)->nullable()->default('0');
-			$table->string(''is_enabled'', 3)->nullable()->default('1');
+			$table->string('cru_csvnote', 500)->nullable();
+			$table->string('is_erpsent', 3)->nullable()->default('0');
+			$table->string('is_enabled', 3)->nullable()->default('1');
 			$table->integer('i')->nullable();
-			$table->integer(''id'', true);
-			$table->string(''code_erp'', 25)->nullable();
-			$table->string(''type'', 15)->nullable();
-			$table->string(''id_tosave'', 25)->nullable();
-			$table->string(''description'', 250)->nullable();
+			$table->integer('id', true);
+			$table->string('code_erp', 25)->nullable();
+			$table->string('type', 15)->nullable();
+			$table->string('id_tosave', 25)->nullable();
+			$table->string('description', 250)->nullable();
 			$table->integer('order_by')->default(100);
 		});
 	}
