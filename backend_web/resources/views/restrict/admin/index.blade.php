476cada9276005c3dd5779327170a38424878da0
ioedumac <eacevedof@yahoo.es>
2020-09-02 22:43:53 +0200

    dashboard con posts

--- backend_web/resources/views/restrict/admin/index.blade.php
+++ backend_web/resources/views/restrict/admin/index.blade.php
@@ -2,19 +2,13 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row">
+        <div class="col-sm-6">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <h5 class="card-title">Posts</h5>
+                    <p class="card-text">CMS</p>
+                    <a href="/adm/posts" class="btn btn-primary">Posts</a>
                 </div>
             </div>
         </div>
