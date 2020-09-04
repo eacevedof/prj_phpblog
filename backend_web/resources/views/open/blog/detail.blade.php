5ef80b0309dc94e782e5bcba749917896fdbb762
ioedumac <eacevedof@yahoo.es>
2020-09-04 20:26:51 +0200

    antes de cambiar la tarjeta del detalle

--- backend_web/resources/views/open/blog/detail.blade.php
+++ backend_web/resources/views/open/blog/detail.blade.php
@@ -9,7 +9,7 @@
     <div class="row">
         <div class="col-md-12">
             <div class="card mb-4 box-shadow">
-                <img class="card-img-top" src="{{$post->url_img1}}" alt="{{$post->title}}">
+                <img class="card-img-top irounded float-left" src="{{$post->url_img1}}" alt="{{$post->title}}">
                 <div class="card-body">
                     <p class="card-text">{{$post->content}}</p>
                     <div class="d-flex justify-content-between align-items-center">
