500b6cd01366136d20316c96b9fdad8822b8ce7b
ioedumac <eacevedof@yahoo.es>
2020-09-04 13:05:02 +0200

    configurando 404

---
+++ backend_web/resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('errors.errors-layout')
+
+@section('content')
+<div class="container">
+    <div class="flex-center position-ref full-height">
+        <div class="code">404</div>
+        <div class="message" style="padding: 10px;">Page not Found</div>
+    </div>
+</div>
+@endsection
