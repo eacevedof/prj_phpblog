3b1457772be3f92d590e99c2c178e49cb6dc558f
ioedumac <eacevedof@yahoo.es>
2020-09-04 13:05:02 +0200

    configurando 404

---
+++ backend_web/resources/views/errors/errors-layout.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('pagetitle')</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="/">Inicio</a>
+
+        </div>
+    </nav>
+    <main class="py-4">
+        @yield('content')
+    </main>
+</div>
+</body>
+</html>
