d552eba99ff18f553777e992d87eb32712bb4554
ioedumac <eacevedof@yahoo.es>
2020-09-05 10:59:28 +0200

    refactor de 404 creo estilo app-btn-blue

--- backend_web/resources/views/errors/404.blade.php
+++ backend_web/resources/views/errors/404.blade.php
@@ -1,10 +1,28 @@
 @extends('errors.errors-layout')
 
+@section("pagetitle","Error 404")
 @section('content')
-<div class="container">
-    <div class="flex-center position-ref full-height">
-        <div class="code">404</div>
-        <div class="message" style="padding: 10px;">Page not Found</div>
+<div id="notfound">
+    <!--
+        https://colorlib.com/wp/free-error-page-templates/
+        https://colorlib.com/etc/404/colorlib-error-404-2/
+    -->
+    <div class="notfound">
+        <div class="notfound-404">
+            <h1>4<span>0</span>4</h1>
+        </div>
+        <h2>The content you requested could not be found!</h2>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/">Inicio</a></li>
+                @auth
+                    <li class="breadcrumb-item" aria-current="page"><a href="/adm">Admin</a></li>
+                @endauth
+                <li class="breadcrumb-item" aria-current="page"><a href="/contacto">Contacto</a></li>
+                <li class="breadcrumb-item" aria-current="page"><a href="/blog">Blog</a></li>
+            </ol>
+        </nav>
     </div>
+
 </div>
 @endsection
--- backend_web/resources/views/errors/errors-layout.blade.php
+++ backend_web/resources/views/errors/errors-layout.blade.php
@@ -4,32 +4,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('pagetitle')</title>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/error.css') }}" rel="stylesheet">
 </head>
 <body>
-<div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="/">Inicio</a>
-            @guest
-                Is guest :S
-            @endguest
-            ||
-            @auth
-                Is auth OOOOOOO
-                <a class="navbar-brand" href="/adm">Admin</a>
-            @endauth
-        </div>
-    </nav>
-    <main class="py-4">
-        @yield('content')
-    </main>
-</div>
+<main>
+    @yield('content')
+</main>
 </body>
 </html>
