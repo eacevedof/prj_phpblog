cdcaa08535affa9c329b04e0dfba5ad247d5e6ac
ioedumac <eacevedof@yahoo.es>
2020-09-05 11:27:27 +0200

    boton azul ok, voy a aplicar bradscrumb en el blog

--- backend_web/resources/views/open/forms/form-search.blade.php
+++ backend_web/resources/views/open/forms/form-search.blade.php
@@ -1,4 +1,4 @@
 <form class="d-flex">
     <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-light" type="submit">Search</button>
+    <button class="btn app-btnblue" type="submit">Search</button>
 </form>
