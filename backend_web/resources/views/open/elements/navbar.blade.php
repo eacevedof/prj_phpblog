7453b3104040f4f5f56de4fe3dbcd342f7ac6113
ioedumac <eacevedof@yahoo.es>
2020-09-05 15:37:34 +0200

    cambio orden en menu

--- backend_web/resources/views/open/elements/navbar.blade.php
+++ backend_web/resources/views/open/elements/navbar.blade.php
@@ -1,16 +1,16 @@
 <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
     <div class="container-fluid">
+        <a class="navbar-brand" href="{{ route('open.home.index') }}"><i class="fa fa-home" aria-hidden="true"></i> / </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarsExample09">
             <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+                <!--
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('open.home.aboutme') }}">Sobre mí</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('open.home.contact') }}">Contacto</a>
-                </li>
+                -->
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
                         Blog
@@ -22,6 +22,9 @@
                         <a class="dropdown-item" href="/blog/python">Python</a>
                         <a class="dropdown-item" href="/blog/docker">Docker</a>
                     </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('open.home.contact') }}">Contacto</a>
                 </li>
                 @auth
                 <li class="nav-item dropdown">
--- backend_web/resources/views/open/open-layout.blade.php
+++ backend_web/resources/views/open/open-layout.blade.php
@@ -9,7 +9,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/png" href="/assets/images/favicon-96x96.png" />
     <!-- Bootstrap CSS -->
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <link href="{{ asset('css/open.css') }}" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 </head>
