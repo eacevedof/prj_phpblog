19a119639ef138538ce45f5ffc7996460e300c1a
ioedumac <eacevedof@yahoo.es>
2020-09-05 11:36:35 +0200

    creo breascrumb.blade.php

---
+++ backend_web/resources/views/open/elements/breadscrumb.blade.php
@@ -0,0 +1,8 @@
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="/">Inicio</a></li>
+    @foreach($breadsrumbs as $scrumb)
+            <li class="breadcrumb-item"><a href="/">Inicio</a></li>
+    @endforeach
+    </ol>
+</nav>
