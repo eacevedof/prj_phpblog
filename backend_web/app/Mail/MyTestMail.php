ac6cf34e30639a10fdec1a1552381e45e980fa99
ioedumac <eacevedof@yahoo.es>
2020-09-05 19:35:11 +0200

    copia del ejemplo envio mail

--- backend_web/app/Mail/MyTestMail.php
+++ backend_web/app/Mail/MyTestMail.php
@@ -10,21 +10,26 @@ class MyTestMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $details;
+
     /**
      * Create a new message instance.
+     *
      * @return void
      */
-    public function __construct()
+    public function __construct($details)
     {
-        //
+        $this->details = $details;
     }
 
     /**
      * Build the message.
+     *
      * @return $this
      */
     public function build()
     {
-        return $this->view('emails.test');
+        return $this->subject('Mail from ItSolutionStuff.com')
+            ->view('emails.test');
     }
 }
