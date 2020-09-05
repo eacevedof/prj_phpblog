1d6e1be3ad1bf148950460056be389cb289cbc2b
ioedumac <eacevedof@yahoo.es>
2020-09-05 17:29:41 +0200

    probando envio email

--- backend_web/tests/Unit/ExampleTest.php
+++ backend_web/tests/Unit/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Support\Facades\Mail;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
@@ -14,5 +15,16 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $this->assertTrue(true);
+    }
+
+    public function test_mail()
+    {
+        $to_name = "Para Mi mismo";
+        $to_email = "";
+        $data = array("name"=>"Ogbonna Vitalis(sender_name)", "body" => "A test mail");
+        Mail::send("emails.mail", $data, function($message) use ($to_name, $to_email) {
+            $message->to($to_email, $to_name)->subject("Laravel Test Mail");
+            $message->from("SENDER_EMAIL_ADDRESS","Test Mail");
+        });
     }
 }
