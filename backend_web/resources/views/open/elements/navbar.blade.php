7dd98b48cc2fccb76bcf01afb02f656e508724f5
ioedumac <eacevedof@yahoo.es>
2020-09-05 15:18:01 +0200

    _get_category abort

--- backend_web/resources/views/open/elements/navbar.blade.php
+++ backend_web/resources/views/open/elements/navbar.blade.php
@@ -1,6 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ route('open.home.index') }}">Inicio</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
