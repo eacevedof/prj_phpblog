ff3465e650d44a02bec2ef337f6a2f9d8719ec65
ioedumac <eacevedof@yahoo.es>
2020-09-05 15:04:53 +0200

    scrumbs

--- backend_web/resources/views/open/elements/breadscrumb.blade.php
+++ backend_web/resources/views/open/elements/breadscrumb.blade.php
@@ -1,11 +1,11 @@
 <nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="/">Inicio</a></li>
+    <ol class="breadcrumb app-breadscrumb">
+        <li class="breadcrumb-item app-breadscrumb-item"><a href="/">Inicio</a></li>
     @php
         $css = "active"
     @endphp
     @foreach($breadscrumb as $scrumb)
-        <li class="breadcrumb-item @if($loop->last) {{$css}} @endif"><a href="{{$scrumb["url"]}}">{{$scrumb["text"]}}</a></li>
+        <li class="breadcrumb-item app-breadscrumb-item @if($loop->last) {{$css}} @endif"><a href="{{$scrumb["url"]}}">{{$scrumb["text"]}}</a></li>
     @endforeach
     </ol>
 </nav>
