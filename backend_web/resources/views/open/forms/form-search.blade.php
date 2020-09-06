bee7b7182ea6228fdd7aa6ced8d9033e6212aa4e
ioedumac <eacevedof@yahoo.es>
2020-09-06 09:37:36 +0200

    comento form-search

--- backend_web/resources/views/open/forms/form-search.blade.php
+++ backend_web/resources/views/open/forms/form-search.blade.php
@@ -1,4 +1,6 @@
-<form class="d-flex">
+<!--
+<form id="form-search" class="d-flex">
     <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search">
     <button class="btn app-btnblue" type="submit">Search</button>
 </form>
+-->
