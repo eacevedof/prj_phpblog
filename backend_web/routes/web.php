152a5a516e3cb07e881ffb314d7353849f35c7ea
ioedumac <eacevedof@yahoo.es>
2020-09-06 12:23:28 +0200

    ya envia desde email/contact

--- backend_web/routes/web.php
+++ backend_web/routes/web.php
@@ -22,7 +22,7 @@ Route::get('/blog/{catslug}/{postslug}','Open\BlogController@detail')->name("ope
 Route::get('/blog/{catslug}','Open\BlogController@category')->name("open.blog.category");
 Route::get('/blog/','Open\BlogController')->name("open.blog.index");
 
-Route::post('/email/contact', "Common\EmailController@contact")-name("common.email.contact");
+Route::get('/email/contact', "Common\Email\EmailController@contact")->name("common.email.contact");
 
 Route::get('/email/send', function () {
 
