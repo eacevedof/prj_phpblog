94d3238c1c9beab5ff61c64b21555f573c21a197
ioedumac <eacevedof@yahoo.es>
2020-09-07 17:40:32 +0200

    home-header.blade.php

--- backend_web/resources/views/open/elements/home-header.blade.php
+++ backend_web/resources/views/open/elements/home-header.blade.php
@@ -3,9 +3,9 @@
         <h5 class="card-title">Blog de Eduardo Acevedo - Desarrollador fullstack</h5>
     </div>
     <div class="card-body">
+        <img src="https://resources.theframework.es/eduardoaf.com/20200906/095050-logo-eduardoafcom_500.png" width="350" class="pull-left mr-3">
         <p class="card-text">
-            <img class="img-fluid pull-left" src="https://resources.theframework.es/eduardoaf.com/20200906/095050-logo-eduardoafcom_500.png" />
-            Artículos de tecnología en general
+
         </p>
         <a href="/blog" class="btn app-btnblue app-btnshadow">Blog</a>
     </div>
