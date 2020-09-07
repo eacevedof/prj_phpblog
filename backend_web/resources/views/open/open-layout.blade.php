72cc6ff5e9eb66ec2e0d162ea7144eafe2282004
ioedumac <eacevedof@yahoo.es>
2020-09-07 16:56:38 +0200

    error: no me coge el css de enlighterjs

--- backend_web/resources/views/open/open-layout.blade.php
+++ backend_web/resources/views/open/open-layout.blade.php
@@ -42,7 +42,8 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
+    <!-- EnlighterJS Resources !-->
+    <link href="{{ asset('enlighter/enlighterjs.min.css') }}" rel="stylesheet" />
     <link href="{{ asset('css/open.css') }}" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 </head>
