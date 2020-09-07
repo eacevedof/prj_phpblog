885817a1aeca9590141ba5554451cb053171b790
ioedumac <eacevedof@yahoo.es>
2020-09-07 13:44:18 +0200

    footer ii

---
+++ backend_web/resources/views/open/elements/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="text-center pt-3 pb-1 app-footer">
+    <p class="text-white text-bold">
+        <a href="/" class="text-white">Inicio</a> |
+        <a href="/blog" class="text-white">Blog</a> |
+        <a href="/contacto" class="text-white">Contacto</a> |
+        <a href="https://twitter.com/eacevedof" class="text-white">Twitter: @eacevedof</a>.
+    </p>
+</footer>
--- backend_web/resources/views/open/open-layout.blade.php
+++ backend_web/resources/views/open/open-layout.blade.php
@@ -61,12 +61,7 @@
         </div>
     </div>
 </main>
-<footer class="mt-auto text-dark-50 text-center">
-    <p>
-        Cover template for <a href="https://getbootstrap.com/" class="text-dark">Bootstrap</a>,
-        by <a href="https://twitter.com/mdo" class="text-dark">@mdo</a>.
-    </p>
-</footer>
+@include("open.elements.footer")
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
