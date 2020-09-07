63f217a634be81bc95dd57c1fb1b8227c3e9dd57
ioedumac <eacevedof@yahoo.es>
2020-09-07 17:59:00 +0200

    texto home-header

--- backend_web/resources/views/open/elements/home-header.blade.php
+++ backend_web/resources/views/open/elements/home-header.blade.php
@@ -1,10 +1,18 @@
 <div class="card app-card">
     <div class="card-header pt-3 app-card-bb">
-        <h5 class="card-title">Blog de Eduardo Acevedo - Desarrollador fullstack</h5>
+        <h5 class="card-title">Blog de Eduardo Acevedo</h5>
     </div>
     <div class="card-body">
         <img src="https://resources.theframework.es/eduardoaf.com/20200906/095050-logo-eduardoafcom_500.png" width="350" class="pull-left mr-3">
         <p class="card-text">
+            Soy desarrollador Fullstack especializado en lenguajes de backend <b>PHP</b> y <b>Python</b>. <br/><br/>
+            Llevo más de 15 años como informático en distintas áreas, esto me ha permitido trabajar con varias
+            tecnologías, programas y lenguajes de programación.<br/><br/>
+
+            Originalmente empecé con <b>Visual Basic 6</b> y <b>Microsoft Access</b>, posteriormente apareció <b>c#</b> y como este, entonces,
+            era muy parecido a Java con lo cual pude crear un proyecto básico con los <b>Java Beans</b> y <b>JSP</b>. <br/><br/>
+
+            En esta época esta muy de moda <b>Flash</b> y <b></b>.
 
         </p>
         <a href="/blog" class="btn app-btnblue app-btnshadow">Blog</a>
