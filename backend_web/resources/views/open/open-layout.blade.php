7ad2a3e2602686326baabc66acebbf8ecfa3083e
ioedumac <eacevedof@yahoo.es>
2020-09-07 11:00:32 +0200

    open-layout seo metas

--- backend_web/resources/views/open/open-layout.blade.php
+++ backend_web/resources/views/open/open-layout.blade.php
@@ -7,7 +7,38 @@
     <meta name="description" content="@yield("pagedescription")">
     <meta name="keywords" content="@yield("pagekeywords")">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- tpas -->
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta http-equiv="Content-Language" content="es_ES">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+    <meta name="author" content="Eduardo Acevedo F"/>
+    <meta name="Distribution" content="Global"/>
+    <meta name="Rating" content="General"/>
+    <meta name="security" content="public" />
+    <meta http-equiv="Cache-Control" content="s-maxage = 3600, max-age = 60, private" />
+
+
     <link rel="icon" type="image/png" href="https://resources.theframework.es/eduardoaf.com/20200905/160818-favicon.png"/>
+<!--
+    open graph protocol
+    <meta property="og:title" content=" same as title" />
+    <meta property="og:description" content=" same as desc" />
+    <meta property="og:locale" content="es_ES" />
+    <meta property="og:url" content="https://www.eduardoaf.com//s" />
+	<meta property="og:site_name" content="www.eduardoaf.com" />
+
+    <link rel="shortcut icon" href="/img/favicon.ico">
+    <link rel="apple-touch-icon" href="/img/apple_icons_57x57.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/img/apple_icons_72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/img/apple_icons_114x114.png">
+
+    canonical:
+	<link rel="next" href="https://www.eduardoaf.com//s/2" />
+    <link rel="canonical" href="https://www.eduardoaf.com/s" />
+
+    Robots:
+    <meta name="robots" content="noindex,follow"/>
+-->
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
