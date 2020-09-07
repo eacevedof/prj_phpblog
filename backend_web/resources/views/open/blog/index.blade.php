d85dd3a3993b1c93098fcc843b9446589e05c31f
ioedumac <eacevedof@yahoo.es>
2020-09-07 16:17:45 +0200

    hashlinks

--- backend_web/resources/views/open/blog/index.blade.php
+++ backend_web/resources/views/open/blog/index.blade.php
@@ -9,9 +9,14 @@
 <p>
     <small>Total: {{$result->count()}}</small>
 </p>
-<p>
-    <a href="/blog/javascript"
-</p>
+<div class="row mb-2">
+    <div class="col-12">
+        @foreach($submenublog as $item)
+            <a class="float-left ml-1 app-hashlink" href="{{$item->slug}}">#{{strtolower($item->description)}}</a>
+        @endforeach
+    </div>
+</div>
+
 <div class="row">
     @foreach ($result as $post)
         <div class="col-md-4">
