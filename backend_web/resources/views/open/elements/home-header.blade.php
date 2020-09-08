8344b3f45ec9f1c2434f6515480cc9441827b22f
ioedumac <eacevedof@yahoo.es>
2020-09-08 12:02:49 +0200

    home-header.blade.php

--- backend_web/resources/views/open/elements/home-header.blade.php
+++ backend_web/resources/views/open/elements/home-header.blade.php
@@ -3,7 +3,7 @@
         <h5 class="card-title">Blog de Eduardo Acevedo</h5>
     </div>
     <div class="card-body">
-        <img src="https://resources.theframework.es/eduardoaf.com/20200906/095050-logo-eduardoafcom_500.png" width="350" class="pull-left mr-3">
+        <img src="https://resources.theframework.es/eduardoaf.com/20200906/095050-logo-eduardoafcom_500.png" width="400" class="pull-left mr-3">
         <p class="card-text">
             Soy desarrollador Fullstack especializado en lenguajes backend <b>PHP</b> y <b>Python</b>. <br/><br/>
             Llevo más de 15 años como informático lo que ha permitido involucrarme en distintas áreas. Desde hacer cableados de red
@@ -21,15 +21,15 @@
             En esta época era muy común encontrar webs con <b>Macromedia Flash</b>, <b>Actionscript</b> y <b>PHP</b>. Trabajando en una gencia de publicidad me enfrenté
             al desarrollo en este <strong>stack</strong>. Hacía trabajos muy básicos como enviar emails, gestionar algún bug en <b>Joomla</b> y <b>Wordpress</b>.
             entonces mi forma de desarrollar era el típico "código espagueti". No obstante intenté replicar el MVC pero sin clases, todo con funciones, un poco al estilo wordpress.
-            Esto me sirvió para entender un poco mejor PHP desde cero.  El uso de include y require, php.ini, variables globales, conexión con mysql y la interactividad con js.
+            Esto me sirvió para entender mejor PHP desde cero.  El uso de include y require, php.ini, variables globales, conexión con mysql y la interactividad con js.
             <br/><br/>
-            Cambio de empresa. En esta se usa <b>Cake PHP</b>, <b>Mysql</b>, <b>Jquery</b>, <b>Wordpress</b> y <b>SVN</b> en un servidor dedicado con <b>Linux Centos</b>.
+            Cambio de empresa. En esta nueva experiencia es un stack más robusto y esperable: <b>Cake PHP</b>, <b>Mysql</b>, <b>Jquery</b>, <b>Wordpress</b> y <b>SVN</b> en un servidor dedicado con <b>Linux Centos</b>.
             Mi grata sorpresa es que Cake replica MVC con POO lo cual me lleva a replantearme el proyecto anterior usando clases imitando a este framework pero sin su ORM.
             Termino desarrollando la primera versión de: <a href="https://theframework.es" rel="nofollow">theframework.es</a>
             <br/><br/>
-            Desde entonces hasta hoy he seguido trabajando con PHP tanto en
+            Desde entonces hasta hoy he seguido trabajando con PHP no obstante cada empresa tiene su forma de trabajar.
         </p>
-        <a href="/blog" class="btn app-btnblue app-btnshadow">Blog</a>
+        <a href="/contacto" class="btn app-btnblue app-btnshadow">Contacta conmigo</a>
     </div>
     <div class="card-footer text-muted text-right app-card-bt">
         <b>Última actualización:</b> {{get_dmy_hi($updatedat)}}
