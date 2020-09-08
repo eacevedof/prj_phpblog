192b6df0400b0c077010e16168a17ad9f408b200
ioedumac <eacevedof@yahoo.es>
2020-09-08 16:20:03 +0200

    fix url_final en blade

--- backend_web/resources/views/open/blog/index.blade.php
+++ backend_web/resources/views/open/blog/index.blade.php
@@ -24,7 +24,7 @@
                     </p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <a href="/blog{{$post->url_final}}" type="button" class="btn btn-md app-btnblue app-btnshadow">Leer más...</a>
+                            <a href="{{$post->url_final}}" type="button" class="btn btn-md app-btnblue app-btnshadow">Leer más...</a>
                             @auth
                                 <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
                                 <a href="/adm/post/update/{{$post->id}}" target="_blank" class="btn btn-sm btn-primary">Edit ({{$post->id}})</a>
