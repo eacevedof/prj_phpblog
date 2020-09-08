a234d85a0ba18a37f45e56258b211976106c15f6
ioedumac <eacevedof@yahoo.es>
2020-09-08 12:53:47 +0200

    home-header.blade.php

--- backend_web/resources/views/open/elements/home-header.blade.php
+++ backend_web/resources/views/open/elements/home-header.blade.php
@@ -6,13 +6,13 @@
         <img src="https://resources.theframework.es/eduardoaf.com/20200906/095050-logo-eduardoafcom_500.png" width="300" class="pull-left mr-3 debug">
         <p class="card-text">
             Soy desarrollador Fullstack especializado en lenguajes backend <b>PHP</b> y <b>Python</b>. <br/><br/>
-            Llevo más de 15 años como informático lo que ha permitido involucrarme en distintas áreas. Desde hacer cableados de red
+            Llevo más de 15 años como informático lo que me ha permitido involucrarme en distintas áreas. Desde hacer cableados de red
             pasando por diseño gráfico, programación e incluso mover campañas en Facebook.
             Por este motivo he podido trabajar con varias tecnologías, programas y lenguajes de programación.
             <br/><br/>
             Originalmente empecé con <b>Visual Basic 6</b>, <b>Microsoft Access</b> y <b>VBA</b>. Posteriormente apareció <b>C#</b> y me dediqué
             a aprenderlo apoyandome en el libro <i>"Visual C# .Net de Anaya"</i> pudiendo realizar dos proyectos uno personal y otro en una agencia de viajes.
-            Entonces casi todas mis busquedas sobre dudas acababan en la web de <i>"el guille (elguille.info)"</i> con excelente contenido en lo que a <b>.NET</b>
+            Entonces casi todas mis busquedas de dudas sobre VB y C# acababan en la web de <i>"el guille (elguille.info)"</i> con excelente contenido en lo que a <b>.NET</b>
             se refiere.
             <br/><br/>
             C# me sirvió para aplicar por primera vez el paradigma de la programación orientada a objetos (OOP en inglés).<br/>
@@ -20,14 +20,25 @@
             <br/><br/>
             En esta época era muy común encontrar webs con <b>Macromedia Flash</b>, <b>Actionscript</b> y <b>PHP</b>. Trabajando en una gencia de publicidad me enfrenté
             al desarrollo en este <strong>stack</strong>. Hacía trabajos muy básicos como enviar emails, gestionar algún bug en <b>Joomla</b> y <b>Wordpress</b>.
-            entonces mi forma de desarrollar era el típico "código espagueti". No obstante intenté replicar el MVC pero sin clases, todo con funciones, un poco al estilo wordpress.
+            Entonces mi forma de desarrollar era el típico "código espagueti". No obstante intenté replicar el MVC pero sin clases, todo con funciones, un poco al estilo wordpress.
             Esto me sirvió para entender mejor PHP desde cero.  El uso de include y require, php.ini, variables globales, conexión con mysql y la interactividad con js.
             <br/><br/>
             Cambio de empresa. En esta nueva experiencia es un stack más robusto y esperable: <b>Cake PHP</b>, <b>Mysql</b>, <b>Jquery</b>, <b>Wordpress</b> y <b>SVN</b> en un servidor dedicado con <b>Linux Centos</b>.
             Mi grata sorpresa es que Cake replica MVC con POO lo cual me lleva a replantearme el proyecto anterior usando clases imitando a este framework pero sin su ORM.
             Termino desarrollando la primera versión de: <a href="https://theframework.es" rel="nofollow">theframework.es</a>
             <br/><br/>
-            Desde entonces hasta hoy he seguido trabajando con PHP no obstante cada empresa tiene su forma de trabajar.
+            Desde entonces hasta hoy sigo trabajando con PHP, sin dejar de hacer seguimiento a la evolución de otras técnologías. Tales como:
+            <ul>
+                <li>PHP, Yii 2, Symfony 5 y Laravel 7, REST</li>
+                <li>Python, Django, Flask y Selenium</li>
+                <li>SQL Server, Mysql, MongoDB, Hive y Redis</li>
+                <li>Vanilla Js, jQuery, Rxjs, VueJs, Svelte y ReactJs</li>
+                <li>CSS, Vuetify, Bootstrap y Bulma</li>
+                <li>Docker y Vagrant</li>
+                <li>Flutter y Android</li>
+                <li>Figma</li>
+                <li>.Net Core</li>
+            </ul>
         </p>
         <a href="/contacto" class="btn app-btnblue app-btnshadow">Contacta conmigo</a>
     </div>
