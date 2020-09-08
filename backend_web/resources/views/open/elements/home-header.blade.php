87cf3dbdebb41b9cea5e518508fb0a3da908b13e
ioedumac <eacevedof@yahoo.es>
2020-09-08 13:45:46 +0200

    mejora tamaños input contact.blade.php

--- backend_web/resources/views/open/elements/home-header.blade.php
+++ backend_web/resources/views/open/elements/home-header.blade.php
@@ -3,12 +3,12 @@
         <h5 class="card-title">Blog de Eduardo Acevedo</h5>
     </div>
     <div class="card-body">
-        <img src="https://resources.theframework.es/eduardoaf.com/20200906/095050-logo-eduardoafcom_500.png" width="300" class="pull-left mr-3 debug">
+        <img src="https://resources.theframework.es/eduardoaf.com/20200906/095050-logo-eduardoafcom_500.png" width="450" class="pull-left mr-3 img-fluid">
         <p class="card-text">
             Soy desarrollador Fullstack especializado en lenguajes backend <b>PHP</b> y <b>Python</b>. <br/><br/>
             Llevo más de 15 años como informático lo que me ha permitido involucrarme en distintas áreas. Desde hacer cableados de red
             pasando por diseño gráfico, programación e incluso mover campañas en Facebook.
-            Por este motivo he podido trabajar con varias tecnologías, programas y lenguajes de programación.
+            Por este motivo he podido trabajar con varias tecnologías, programas y lenguajes de programación. Siendo el desarrollo web mi faceta más destacable.
             <br/><br/>
             Originalmente empecé con <b>Visual Basic 6</b>, <b>Microsoft Access</b> y <b>VBA</b>. Posteriormente apareció <b>C#</b> y me dediqué
             a aprenderlo apoyandome en el libro <i>"Visual C# .Net de Anaya"</i> pudiendo realizar dos proyectos uno personal y otro en una agencia de viajes.
@@ -21,7 +21,7 @@
             En esta época era muy común encontrar webs con <b>Macromedia Flash</b>, <b>Actionscript</b> y <b>PHP</b>. Trabajando en una gencia de publicidad me enfrenté
             al desarrollo en este <strong>stack</strong>. Hacía trabajos muy básicos como enviar emails, gestionar algún bug en <b>Joomla</b> y <b>Wordpress</b>.
             Entonces mi forma de desarrollar era el típico "código espagueti". No obstante intenté replicar el MVC pero sin clases, todo con funciones, un poco al estilo wordpress.
-            Esto me sirvió para entender mejor PHP desde cero.  El uso de include y require, php.ini, variables globales, conexión con mysql y la interactividad con js.
+            Esto me sirvió para entender mejor PHP desde cero.  El uso de include, require, php.ini, variables globales, conexión con mysql y la interactividad con js.
             <br/><br/>
             Cambio de empresa. En esta nueva experiencia es un stack más robusto y esperable: <b>Cake PHP</b>, <b>Mysql</b>, <b>Jquery</b>, <b>Wordpress</b> y <b>SVN</b> en un servidor dedicado con <b>Linux Centos</b>.
             Mi grata sorpresa es que Cake replica MVC con POO lo cual me lleva a replantearme el proyecto anterior usando clases imitando a este framework pero sin su ORM.
@@ -29,7 +29,7 @@
             <br/><br/>
             Desde entonces hasta hoy sigo trabajando con PHP, sin dejar de hacer seguimiento a la evolución de otras técnologías. Tales como:
             <ul>
-                <li>PHP, Yii 2, Symfony 5 y Laravel 7, REST</li>
+                <li>PHP 7 y 8, Yii 2, Symfony 5 y Laravel 7, REST</li>
                 <li>Python, Django, Flask y Selenium</li>
                 <li>SQL Server, Mysql, MongoDB, Hive y Redis</li>
                 <li>Vanilla Js, jQuery, Rxjs, VueJs, Svelte y ReactJs</li>
@@ -38,9 +38,14 @@
                 <li>Flutter y Android</li>
                 <li>Figma</li>
                 <li>.Net Core</li>
+                <li>Windows, Mac OS, Linux</li>
             </ul>
+            La idea de este blog es plasmar todas aquellas experiencias y soluciones relevantes que voy experimentando
+            en el uso de dichas herramientas.
         </p>
-        <a href="/contacto" class="btn app-btnblue app-btnshadow">Contacta conmigo</a>
+        <p class="text-right">
+            <a href="/contacto" class="btn app-btnblue app-btnshadow">Contacta conmigo</a>
+        </p>
     </div>
     <div class="card-footer text-muted text-right app-card-bt">
         <b>Última actualización:</b> {{get_dmy_hi($updatedat)}}
