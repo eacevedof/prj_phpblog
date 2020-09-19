b97d866a65783f8affa6c222ce8afa207ed74003
ioedumac <eacevedof@yahoo.es>
2020-09-19 15:11:16 +0200

    opn-breadscrumb

--- backend_web/resources/views/open/elements/breadscrumb.blade.php
+++ backend_web/resources/views/open/elements/breadscrumb.blade.php
@@ -1,11 +1,11 @@
 <nav aria-label="breadcrumb">
-    <ol class="breadcrumb app-breadscrumb">
-        <li class="breadcrumb-item app-breadscrumb-item"><a href="/">Inicio</a></li>
+    <ol class="breadcrumb opn-breadscrumb">
+        <li class="breadcrumb-item opn-breadscrumb-item"><a href="/">Inicio</a></li>
     @php
         $css = "active"
     @endphp
     @foreach($breadscrumb as $scrumb)
-        <li class="breadcrumb-item app-breadscrumb-item @if($loop->last) {{$css}} @endif"><a href="{{$scrumb["url"]}}">{{$scrumb["text"]}}</a></li>
+        <li class="breadcrumb-item opn-breadscrumb-item @if($loop->last) {{$css}} @endif"><a href="{{$scrumb["url"]}}">{{$scrumb["text"]}}</a></li>
     @endforeach
     </ol>
 </nav>
