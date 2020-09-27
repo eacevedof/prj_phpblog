6447f90eafebb060c19cba16456253d06514dc9d
ioedumac <eacevedof@yahoo.es>
2020-09-27 13:04:43 +0200

    bg-success en hashlinks

--- backend_web/resources/views/open/elements/hashlinks.blade.php
+++ backend_web/resources/views/open/elements/hashlinks.blade.php
@@ -3,7 +3,7 @@
 <div class="row mb-2">
     <div class="col-12">
         @foreach($submenublog as $item)
-            <a class="badge bg-secondary float-left ml-1 opn-hashlink" href="{{$item->url_final}}">#{{strtolower($item->description)}}</a>
+            <a class="badge bg-success float-left ml-1 opn-hashlink" href="{{$item->url_final}}">#{{strtolower($item->description)}}</a>
         @endforeach
     </div>
 </div>
