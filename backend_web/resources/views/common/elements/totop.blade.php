0fb0f5801e8f21a26886f8684ff96ea45d53bea9
ioedumac <eacevedof@yahoo.es>
2020-09-28 18:56:20 +0200

    fix altura a mostrar btn to-up

--- backend_web/resources/views/common/elements/totop.blade.php
+++ backend_web/resources/views/common/elements/totop.blade.php
@@ -5,10 +5,17 @@
 </div>
 <script type="application/javascript">
 (function() {
+    const $divtop = document.getElementById("div-totop")
+    //window.scrollY + document.querySelector('#elementId').getBoundingClientRect().top // Y
+    //window.scrollX + document.querySelector('#elementId').getBoundingClientRect().left // X
+
     //console.log(`document.documentElement.scrollTop:${document.documentElement.scrollTop}, window.innerHeight:${window.innerHeight}`);
     window.addEventListener("scroll", function() {
-        const $divtop = document.getElementById("div-totop")
-        if(document.documentElement.scrollTop > window.innerHeight) {
+        //const divY = window.scrollY + $divtop.getBoundingClientRect().top
+        //console.log("scrolltop:", document.documentElement.scrollTop, "scrolly", window.scrollY)
+        //console.log("divtop.y:",$divtop.getBoundingClientRect().top,"scrolltop",document.documentElement.scrollTop)
+        //if(document.documentElement.scrollTop > window.innerHeight){
+        if(document.documentElement.scrollTop > 115){
             $divtop.style.display = "inherit"
         }
         else {
