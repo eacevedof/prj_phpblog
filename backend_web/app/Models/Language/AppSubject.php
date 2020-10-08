891182166e8d60b1247da106a08e97a769e94ebf
ioedumac <eacevedof@yahoo.es>
2020-10-08 19:25:50 +0200

    appsubject model

--- backend_web/app/Models/Language/AppSubject.php
+++ backend_web/app/Models/Language/AppSubject.php
@@ -1,9 +1,4 @@
 <?php
-
-/**
- * Created by Reliese Model.
- */
-
 namespace App\Models\Language;
 
 use Carbon\Carbon;
@@ -32,47 +27,52 @@ use Illuminate\Database\Eloquent\Model;
  * @property string|null $source
  * @property string|null $url_resource
  * @property int|null $id_type_source
+ * @property string|null $seo_title
+ * @property string|null $seo_description
+ * @property string|null $seo_keywords
  * @property string|null $code_cache
  *
  * @package App\Models\Language
  */
 class AppSubject extends Model
 {
-	protected $table = 'app_subject';
-	public $timestamps = false;
+    protected $table = 'app_subject';
+    public $timestamps = false;
 
-	protected $casts = [
-		'i' => 'int',
-		'id_type_source' => 'int'
-	];
+    protected $casts = [
+        'i' => 'int',
+        'id_type_source' => 'int'
+    ];
 
-	protected $dates = [
-		'insert_date',
-		'update_date',
-		'delete_date'
-	];
+    protected $dates = [
+        'insert_date',
+        'update_date',
+        'delete_date'
+    ];
 
-	protected $fillable = [
-		'processflag',
-		'insert_platform',
-		'insert_user',
-		'insert_date',
-		'update_platform',
-		'update_user',
-		'update_date',
-		'delete_platform',
-		'delete_user',
-		'delete_date',
-		'cru_csvnote',
-		'is_erpsent',
-		'is_enabled',
-		'i',
-		'code_erp',
-		'description',
-        'excerpt',
-		'source',
-		'url_resource',
-		'id_type_source',
-		'code_cache'
-	];
+    protected $fillable = [
+        'processflag',
+        'insert_platform',
+        'insert_user',
+        'insert_date',
+        'update_platform',
+        'update_user',
+        'update_date',
+        'delete_platform',
+        'delete_user',
+        'delete_date',
+        'cru_csvnote',
+        'is_erpsent',
+        'is_enabled',
+        'i',
+        'code_erp',
+        'description',
+        'source',
+        'url_resource',
+        'id_type_source',
+        'seo_title',
+        'seo_description',
+        'seo_keywords',
+        'code_cache'
+    ];
 }
