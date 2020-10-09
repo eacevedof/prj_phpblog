74c81789e50c2313383178e54efd23f1f0f5d759
ioedumac <eacevedof@yahoo.es>
2020-10-09 23:14:34 +0200

    practice-left-blade

---
+++ backend_web/resources/views/open/elements/bulma/practice-left.blade.php
@@ -0,0 +1,32 @@
+<div class="column is-3">
+    <aside class="is-medium menu">
+        <p class="menu-label">
+            categories
+        </p>
+        <ul class="menu-list">
+            <li class="is-right"><a href="#const" class="is-active"><i class="fab fa-css3-alt"></i> CSS</a></li>
+            <li><a href="#let" class="is-active"><i class="fab fa-js"></i> JS</a></li>
+            <li><a href="#let" class="is-active"><i class="fab fa-html5"></i> HTML</a></li>
+        </ul>
+        <p class="menu-label">
+            More to read...
+        </p>
+        <ul class="menu-list">
+            <li><span class="tag is-white is-medium">Lorem</span></li>
+            <li><span class="tag is-white is-medium">Ipsum</span></li>
+            <li><span class="tag is-white is-medium">Dolor</span></li>
+            <li><span class="tag is-white is-medium">Animi</span></li>
+            <li><span class="tag is-white is-medium">Eximi</span></li>
+            <li><span class="tag is-white is-medium">Nullius</span></li>
+            <li><span class="tag is-white is-medium">Oxipi</span></li>
+            <li><span class="tag is-white is-medium">Vultus</span></li>
+            <li><span class="tag is-white is-medium">Voluptatis</span></li>
+            <li><span class="tag is-white is-medium">Exomarphis</span></li>
+            <li><span class="tag is-white is-medium">Finimi</span></li>
+            <li><span class="tag is-white is-medium">Aenigma</span></li>
+            <li><span class="tag is-white is-medium">Arkham</span></li>
+            <li><span class="tag is-white is-medium">Blue</span></li>
+            <li><span class="tag is-white is-medium">Medium</span></li>
+        </ul>
+    </aside>
+</div>
