93f59c26d5a02089dedd3980e29f9c9887de740c
ioedumac <eacevedof@yahoo.es>
2020-10-10 21:23:46 +0200

    footer

--- backend_web/resources/views/open/elements/bulma/footer.blade.php
+++ backend_web/resources/views/open/elements/bulma/footer.blade.php
@@ -3,12 +3,12 @@
         <div class="content has-text-centered">
             <a class="lang-text-black" href="{{ route('open.home.index') }}">
                 <i class="fa fa-home"></i>
-            </a> /
-            <a href="/blog">Blog</a> /
-            <a href="/contacto">Contacto</a> /
-            <a href="https://github.com/eacevedof" target="_blank" rel="nofollow"><i class="fa fa-github" aria-hidden="true"></i> Github</a> /
+            </a> <span class="lang-text-white">|</span>
+            <a href="/blog">Blog</a> <span class="lang-text-white">|</span>
+            <a href="/contacto">Contacto</a> <span class="lang-text-white">|</span>
+            <a href="https://github.com/eacevedof" target="_blank" rel="nofollow"><i class="fa fa-github" aria-hidden="true"></i> Github</a> <span class="lang-text-white">|</span>
             <a href="https://twitter.com/eacevedof" target="_blank" rel="nofollow"><i class="fa fa-twitter" aria-hidden="true"></i> Twitter</a>
-            <p>
+            <p class="lang-text-white">
                 <strong class="lang-text-white">eduardoaf.com</strong> by <a href="/">Eduardo Acevedo</a>.<br/>
             </p>
         </div>
