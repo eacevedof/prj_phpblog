b9fcb00575b748da5afea7b38574745c93a667c2
ioedumac <eacevedof@yahoo.es>
2020-10-10 17:09:41 +0200

    fa va mas o menos

--- backend_web/resources/views/open/elements/bulma/footer.blade.php
+++ backend_web/resources/views/open/elements/bulma/footer.blade.php
@@ -6,11 +6,11 @@
             </a> /
             <a href="/blog" class="lang-text-white">Blog</a> /
             <a href="/contacto" class="lang-text-white">Contacto</a> /
-            <a href="https://github.com/eacevedof" target="_blank" rel="nofollow"><i class="far fa-github-alt" aria-hidden="true"></i></a>
-            <a href="https://twitter.com/eacevedof" target="_blank" rel="nofollow"><i class="far fa-twitter" aria-hidden="true"></i></a>
+            <a href="https://github.com/eacevedof" target="_blank" rel="nofollow"><i class="fa fa-github" aria-hidden="true"></i>Github</a> /
+            <a href="https://twitter.com/eacevedof" target="_blank" rel="nofollow"><i class="fa fa-twitter" aria-hidden="true"></i>Twitter</a>
         </div>
         <p>
-            <strong>eduardoaf.com</strong> by <a href="/">Eduardo Acevedo Farje</a>.<br/>
+            <strong>eduardoaf.com</strong> by <a href="/">Eduardo Acevedo</a>.<br/>
         </p>
     </div>
 </footer>
