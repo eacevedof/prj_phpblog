b88b8809442fe1957929e10d2b07beead30cfa9e
ioedumac <eacevedof@yahoo.es>
2020-10-10 22:46:47 +0200

    reset de practice

--- backend_web/resources/views/open/language/practice.blade.php
+++ backend_web/resources/views/open/language/practice.blade.php
@@ -11,16 +11,11 @@ $jsonresult = json_encode($result);
 @section("pagekeywords",$seo["keywords"])
 
 @section("container")
-<link rel="stylesheet" href="https://kingsora.github.io/OverlayScrollbars/etc/os-theme-thin-dark.css">
-<link rel="stylesheet" href="{{asset("assets/bulma-templates/css/prism.css")}}">
-<link rel="stylesheet" href="{{asset("assets/bulma-templates/css/cheatsheet.css")}}">
-
 @include("open.elements.bulma.practice-header")
 <section class="section">
     <div class="container">
         <div class="columns">
             @include("open.elements.bulma.practice-left")
-
             @include("open.elements.bulma.practice-main")
         </div>
     </div>
