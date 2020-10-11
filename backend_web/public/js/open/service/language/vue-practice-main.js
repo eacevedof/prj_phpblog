ad774d37745728001e6b0180d9be4d669f5cc8a0
ioedumac <eacevedof@yahoo.es>
2020-10-11 17:17:11 +0200

    backup question

--- backend_web/public/js/open/service/language/vue-practice-main.js
+++ backend_web/public/js/open/service/language/vue-practice-main.js
@@ -1,17 +1,36 @@
 import funcs from "/js/open/helpers/openfuncs.js"
 import openapi from "/js/open/helpers/openapi.js"
+import db from "/js/open/helpers/opendb.js"
+
+
+const LANGUAGES = {
+    "1":"sp","2":"nl","3":"en"
+}
 
 new Vue({
     el: "#div-practice-main",
     data: {
+        config:{},
+        isfinished: true,
+
+        question:{
+
+        },
+
+        questions:[],
 
     },//data
 
-    async mounted(){
+    mounted(){
+        const config = db.select(LANG_CONFIG)
+        if(config)
+            this.config = {...config}
         console.log("vue-language-practice:",objpractice);
     },//mounted
 
     methods:{
+        start(){
 
+        }
     },//methods
 })
