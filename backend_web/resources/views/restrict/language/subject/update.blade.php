9f709b2c13bc2fb9214b43475a28ca44fa01c0a5
ioedumac <eacevedof@yahoo.es>
2020-10-24 19:30:33 +0200

    tabs

--- backend_web/resources/views/restrict/language/subject/update.blade.php
+++ backend_web/resources/views/restrict/language/subject/update.blade.php
@@ -4,6 +4,20 @@
 <div class="container">
     @section("pagetitle", "update subject")
     @include("restrict/elements/breadscrumb")
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+    </ul>
     <subjectupdate></subjectupdate>
     @include("restrict/elements/breadscrumb")
 </div>
