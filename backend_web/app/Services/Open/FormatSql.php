89514c245f5a321cba57b6ac84a9a8165be302bc
ioedumac <eacevedof@yahoo.es>
2020-12-08 12:15:35 +0100

    backup

--- backend_web/app/Services/Open/FormatSql.php
+++ backend_web/app/Services/Open/FormatSql.php
@@ -27,14 +27,37 @@ class FormatSql extends BaseService
     private function _load_clean()
     {
         $this->clean = [
-            "query" => $this->input["query"] ?? "",
+            "query" => $this->_get_sanitized($this->input["query"] ?? ""),
         ];
+    }
+    private function _get_sanitized($sql)
+    {
+        if(!$sql) return "";
+        $sql = trim($sql);
+        $sql = strtolower($sql);
+        $sql = str_replace("\n"," ",$sql);
+        $sql = str_replace("\r"," ",$sql);
+        $sql = str_replace("    "," ",$sql);
+        $sql = str_replace("   "," ",$sql);
+        $sql = str_replace("  "," ",$sql);
+        return $sql;
+    }
+
+    private function _explode_select()
+    {
+        $sql = $this->clean["query"];
+        $parts = explode(" from ",$sql);
+
+
+        $this->temp["fields"] = explode($parts[0];
+        $this->temp["post-from"] = $parts[1];
+        return $this;
     }
 
     private function _explode_from()
     {
         $sql = $this->clean["query"];
-        $parts = explode("FROM ",$sql);
+        $parts = explode("from ",$sql);
         $this->temp["pre-from"] = $parts[0];
         $this->temp["post-from"] = $parts[1];
         return $this;
@@ -50,16 +73,57 @@ class FormatSql extends BaseService
         return $this;
     }
 
-
-
+    private function _has_part($part, $sql)
+    {
+        if($part=="distinct")//ok
+            return strstr($sql,"select distinct ");
+        elseif($part=="top")
+            return strstr($sql," top ");
+        elseif($part=="where")
+            return strstr($sql," where ");
+        elseif($part=="groupby")
+            return strstr($sql," group by ");
+        elseif($part=="having")
+            return strstr($sql," having ");
+        elseif($part=="orderby")
+            return strstr($sql," order by ");
+        return false;
+    }
 
     private function _get_select()
     {
 
     }
 
-    private function _join()
+    private function _get_top($sql)
     {
+        //puede ser select top o select distinct top
+        $sTopPatern = "/select[\s]+top[\s]+[\d]+[\s]/";
+        preg_match($sTopPatern,$sql,$arMatch);
+        //si no hay coincidencias es probable que haya un distinct asi que se extrae con distinct
+        if(!$arMatch[0])
+        {
+            $sTopPatern = "/select[\s]+distinct[\s]+top[\s]+[\d]+[\s]/";
+            preg_match($sTopPatern,$sql,$arMatch);
+        }
+
+        if($arMatch[0])
+        {
+            $sTop = explode_select("top",$arMatch[0]);
+            $sTop = trim($sTop[1]);
+        }
+
+        return $sTop;
+    }
+
+    private function _explode_query()
+    {
+        $sql = $this->clean["query"];
+        $sql = $this->_get_sanitized($sql);
+
+        $didstinct = $this->_has_part("distinct", $sql);
+        $top = $this->_has_part("top", $sql);
+
 
     }
 
