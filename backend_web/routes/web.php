39766ebf8ae52d2b86b337dc6281db43bc498ea6
ioedumac <eacevedof@yahoo.es>
2020-12-12 17:47:04 +0100

    reloj-alarma-online

--- backend_web/routes/web.php
+++ backend_web/routes/web.php
@@ -37,7 +37,7 @@ Route::get("/servicios/probar-php-preg-match-all","Open\ServiceController@pregma
 Route::post("/services/formatter/sql-query","Open\ServiceController@format_sql")->name("open.service.format_sql");
 Route::get("/servicios/formatear-consulta-sql","Open\ServiceController@formatsql")->name("open.service.formatsql");
 
-Route::get("/servicios/reloj-alarma","Open\ServiceController@alarmclock")->name("open.service.alarmclock");
+Route::get("/servicios/reloj-alarma-online","Open\ServiceController@alarmclock")->name("open.service.alarmclock");
 
 Route::get("/infrastructure/get-max-upload-size","Common\InfrastructureController@get_maxuploadsize")->name("open.service.getmaxuploadsize");
 
