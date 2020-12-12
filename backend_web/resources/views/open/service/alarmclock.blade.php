e83d1d63f6d076dadcde7b13981799fa26b33924
ioedumac <eacevedof@yahoo.es>
2020-12-12 12:45:49 +0100

    vue-alarmclock.blade con sounido

--- backend_web/resources/views/open/service/alarmclock.blade.php
+++ backend_web/resources/views/open/service/alarmclock.blade.php
@@ -12,15 +12,28 @@
     @verbatim
     <div class="card-body">
         <form @submit="on_submit" id="form-alarmclock" class="row g-3">
-            <div class="col-10">
-                <label>SQL:</label>
-                <textarea class="form-control" maxlength="100000" rows="10"
-                          style="font-family: 'Courier New'; font-size: smaller; border:1px solid #00B7FF!important;"
+            <div class="col-1">
+                <label>Horas:</label>
+                <input type="number" class="form-control" min="0" max="300"
                        :disabled="issending"
                        v-model="query"
-                ></textarea>
+                />
             </div>
-            <!-- boton -->
+            <div class="col-1">
+                <label>Minutos:</label>
+                <input type="number" class="form-control" min="0" max="59"
+                       :disabled="issending"
+                       v-model="query"
+                />
+            </div>
+            <div class="col-1">
+                <label>Segundos:</label>
+                <input type="number" class="form-control" min="0" max="59"
+                       :disabled="issending"
+                       v-model="query"
+                />
+            </div>
+            <!-- botones -->
             <div class="col-1">
                 <button id="btn-alarmclock" class="btn btn-dark mt-4" :disabled="issending" >
                     {{btnsend}}
@@ -33,6 +46,11 @@
                     <img v-if="issending" src="/assets/images/loading-bw.gif" width="25" height="25"/>
                 </button>
             </div>
+            <!-- /botones-->
+            <audio id="audio" controls>
+                <source type="audio/mp3" src="https://resources.theframework.es/eduardoaf.com/20201212/124331-alarm-sound-001.mp3" />
+            </audio>
+
             <div v-if="result" class="col-12">
                 <label><b>Resultado:</b> <span style="color: #00B7FF; cursor: pointer;"><i class="fa fa-clipboard" v-on:click="to_clipboard"></i></span></label>
                 <br/><br/>
