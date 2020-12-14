a32744b42277b6a9a07cb056e6e03cee5306d646
ioedumac <eacevedof@yahoo.es>
2020-12-14 20:34:43 +0100

    sslencrypt.js

---
+++ backend_web/public/js/open/service/vue-sslencrypt.js
@@ -0,0 +1,77 @@
+import openapi from "/js/open/helpers/openapi.js"
+import funcs from "/js/open/helpers/openfuncs.js"
+
+Vue.use(VueToast, {position:"top"})
+
+const app = new Vue({
+    el: "#form-pregmatchall",
+    data: {
+        issending: false,
+        btnsend: "Probar",
+
+        pattern: "",
+        flags: "",
+        final: "//",
+        text: "",
+        result: "",
+    },
+
+    mounted(){
+        this.$refs.pattern.focus()
+    },
+
+    methods:{
+        reset(){
+            this.pattern = ""
+            this.flags = ""
+            this.final = ""
+            this.text = ""
+            this.result = ""
+        },
+
+        to_clipboard(){
+            funcs.to_clipboard(this.final)
+            const html = funcs.html_entities(this.final)
+            this.$toast.open(`Pattern: <b>${html}</b> now in clipboard`)
+        },
+
+        update_final(){
+            const pattern = `/${this.pattern}/${this.flags}`
+            this.final = pattern
+        },
+
+        on_submit: async function(e) {
+            e.preventDefault()
+            if(!this.pattern) return
+            if(!this.text) return
+
+            this.issending = true
+            this.btnsend = "Procesando..."
+            this.result = ""
+
+            const response = await openapi.post_pregmatchaall({
+                pattern: this.pattern,
+                flags: this.flags,
+                text: this.text
+            })
+
+            console.log("R",response)
+
+            if(typeof response.error != "undefined"){
+                Swal.fire({
+                    icon: 'error',
+                    html: `Ha ocurrido un error.<br/> Motivo: <br/>
+                           <b>${response.error}</b>`
+                })
+            }
+            else{
+                this.result = response.data
+            }
+
+            this.issending = false
+            this.btnsend = "Probar"
+            this.$nextTick(() => this.$refs.pattern.focus())
+        }//on_submit
+
+    },//methods
+})
