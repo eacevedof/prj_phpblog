6f9992ee1d3eea1a7a1e1f19dd06f586508c106e
ioedumac <eacevedof@yahoo.es>
2021-01-09 18:59:39 +0100

    ya forma todas las partes

--- backend_web/app/Component/FetchComponent.php
+++ backend_web/app/Component/FetchComponent.php
@@ -16,78 +16,79 @@ class FetchComponent
     private $gets    = [];
     private $posts   = [];
 
-    private $request_url;
+    private $request_uri;
     private $objresource = null;
 
-    public function __construct($request_url="")
+    public function __construct($request_uri="")
     {
         if(!in_array("curl",get_loaded_extensions()))
             throw new \Exception("Curl extension not installed",503);
-        $this->request_url = $request_url;
+        $this->request_uri = $request_uri;
     }
 
     private function _curl_setopts()
     {
-        if($this->headers)
-            $this->_header_opts();
-
+        curl_setopt($this->objresource, CURLOPT_RETURNTRANSFER, true);
         foreach ($this->options as $opt => $value)
             curl_setopt($this->objresource, $opt, $value);
 
-        if($this->gets)
-            $this->_get_opts();
-
-        if($this->posts)
-            $this->_post_opts();
+        if($this->headers) $this->_header_opts();
+        if(!$this->posts) $this->_get_opts();
+        if($this->posts) $this->_post_opts();
 
         return $this;
     }
 
     private function _header_opts()
     {
-        $headers = array_walk($this->headers, function (&$k,$v) {
-            $k="$k: $v";
-        });
+        $headers = [];
+        foreach ($this->headers as $k => $v)
+            $headers[] = "$k: $v";
 
         curl_setopt($this->objresource, CURLOPT_HTTPHEADER, $headers);
     }
 
-    private function _get_opts()
+    private function _get_opts(){curl_setopt($this->objresource, CURLOPT_URL, $this->request_uri);}
+
+    private function _post_opts()
     {
-        $geturl = $this->request_url . "?" . http_build_query($this->gets);
-        curl_setopt($this->objresource, CURLOPT_URL, $geturl);
-        curl_setopt($this->objresource, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->objresource, CURLOPT_POST, 1);
+        $urlpost = http_build_query($this->posts);
+        curl_setopt($this->objresource, CURLOPT_POSTFIELDS, $urlpost);
     }
 
-    private function _post_opts(){curl_setopt($this->objresource, CURLOPT_POSTFIELDS, $this->posts);}
-
-    private function _load_objresource()
+    private function _init_uri()
     {
-        $this->objresource = $this->posts ? curl_init($this->request_url): curl_init();
+        if(!$this->request_uri) throw new \Exception("Missing request_uri");
+        if($this->gets) $this->request_uri = $this->request_uri . "?" . http_build_query($this->gets);
+        return $this;
+    }
+
+    private function _init_resource()
+    {
+        $this->objresource = $this->posts ? curl_init($this->request_uri): curl_init();
         return $this;
     }
 
     public function get()
     {
         //configura objresource
-        $this->_load_objresource()
-            //si hay posts crea la opción para este fin
+        $this->_init_uri()      //inicia req_uri con query string si procede
+            ->_init_resource()  //a partir de req_uri y post crea un recurso vacio o con url
             ->_curl_setopts();
 
         $r = curl_exec($this->objresource);
-        dd($r);
-        //dd($this->objresource);
         curl_close($this->objresource);
         return $r;
     }
 
-    public function set_request_uri($url){$this->request_url = $url; return $this;}
+    public function set_request_uri($url){$this->request_uri = $url; return $this;}
 
-    public function add_header($k,$v){$this->headers[]="$k=$v"; return $this;}
+    public function add_header($k,$v){$this->headers[$k]=$v; return $this;}
 
-    public function add_get($k,$v){$this->gets[]="$k=$v"; return $this;}
+    public function add_get($k,$v){$this->gets[$k]=$v; return $this;}
 
-    public function add_post($k,$v){$this->posts[]="$k=$v"; return $this;}
+    public function add_post($k,$v){$this->posts[$k]=$v; return $this;}
 
     public function reset_header(){$this->headers=[]; return $this;}
 
