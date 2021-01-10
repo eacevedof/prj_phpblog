f56488bdcb63f2ea9aed4f86514f57ab2bcc96dc
ioedumac <eacevedof@yahoo.es>
2021-01-10 12:53:29 +0100

    upload-service

--- backend_web/app/Services/Common/UploadService.php
+++ backend_web/app/Services/Common/UploadService.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Services\Common;
 
-use App\Component\CurlComponent as Curl;
+use App\Component\FetchComponent as Fetch;
 use App\Services\BaseService;
 
 class UploadService extends BaseService
@@ -28,15 +28,16 @@ class UploadService extends BaseService
     public function get_token()
     {
         $url = $this->get_env("API_UPLOAD_URL");
-        $curl = new Curl($url);
-        $curl->add_post("user",$this->get_env("API_UPLOAD_USERNAME"));
-        $curl->add_post("password",$this->get_env("API_UPLOAD_PASSWORD"));
-        $curl->add_post("remoteip",$_SERVER["REMOTE_ADDR"]);
-        $curl->add_post("remotehost",$this->_get_origin());
-        $curl->request_post();
-        $r = $curl->get_response();
-        //$this->logd($r,"get_token raw response");
-        $r = \json_decode($r,1);
+
+        $fetch = new Fetch($url);
+        $fetch
+            ->add_post("user", $this->get_env("API_UPLOAD_USERNAME"))
+            ->add_post("password", $this->get_env("API_UPLOAD_PASSWORD"))
+            ->add_post("remoteip", $_SERVER["REMOTE_ADDR"])
+            ->add_post("remotehost", $this->_get_origin())
+        ;
+
+        $r = $fetch->get_array();
         $this->logd($r,"curl.upload.r");
         return $r["data"]["token"] ?? "";
     }
