222264968950a77b7adebb5b10931b277eede8fb
ioedumac <eacevedof@yahoo.es>
2021-02-16 21:13:01 +0100

    reseteando

--- backend_web/resources/views/open/service/electronic/ohmslaw.blade.php
+++ backend_web/resources/views/open/service/electronic/ohmslaw.blade.php
@@ -10,7 +10,6 @@
             <h2 class="card-title mt-2">{{$seo["description"]}}</h2>
             <p>
                 <b>Nota:</b>
-
             </p>
         </div>
         @verbatim
@@ -19,61 +18,43 @@
                     <div class="row d-flex justify-content-xl-center">
                         <div class="col-12">
                             <pre class="alert-info p-3"><b>{{strhh}}</b>:<b>{{strmm}}</b>:<b>{{strss}}</b> <span v-if="seconds && !isstarted">{{seconds}} seg.</span> <span v-if="isstarted">{{hhmmss}}</span></pre>
-                            <div v-if="isstarted" class="progress">
-                                <div class="progress-bar progress-bar-striped bg-warning progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"
-                                     :style="{width: percent}"
-                                >
-                                    <span style="color:black; font-weight: bolder">Tiempo restante {{seconds}} seg.</span>
-                                </div>
-                            </div>
                         </div>
                         <!-- inputs -->
                         <div class="col-4 col-sm-3 col-md-2 col-xl-1">
                             <label>V:</label>
-                            <input type="number" class="form-control" min="0" max="300" maxlength="3"
+                            <input type="number" class="form-control" min="0" max="9999999" maxlength="3"
                                    ref="v"
-                                   :disabled="isstarted"
                                    v-model="v"
-                                   v-on:change="hh_onchange"
+                                   v-on:change="v_onchange"
                                    v-on:focus="$event.target.select()"
                             />
                         </div>
                         <div class="col-1 col-sm-1 col-md-1 col-xl-1 pt-4 m-0">
-                            =
+                            <b>=</b>
                         </div>
                         <div class="col-4 col-sm-3 col-md-2 col-xl-1">
                             <label>I:</label>
-                            <input type="number" class="form-control" min="0" max="59" maxlength="2"
-                                   :disabled="isstarted"
+                            <input type="number" class="form-control" min="0" max="9999999" maxlength="2"
                                    v-model="i"
-                                   v-on:change="mm_onchange"
+                                   v-on:change="i_onchange"
                                    v-on:focus="$event.target.select()"
                             />
                         </div>
+                        <div class="col-1 col-sm-1 col-md-1 col-xl-1 pt-4 m-0">
+                            <b>x</b>
+                        </div>
                         <div class="col-4 col-sm-3 col-md-2 col-xl-1">
                             <label>R:</label>
-                            <input type="number" class="form-control" min="0" max="59" maxlength="2"
-                                   :disabled="isstarted"
+                            <input type="number" class="form-control" min="0" max="9999999" maxlength="2"
                                    v-model="r"
-                                   v-on:change="ss_onchange"
+                                   v-on:change="i_onchange"
                                    v-on:focus="$event.target.select()"
                             />
                         </div>
                     </div>
                     <!-- botones -->
                     <div class="row d-flex justify-content-xl-center">
-                        <div class="col-4 col-sm-3 col-md-2 col-xl-1">
-                            <button type="button"  class="btn btn-info mt-4"
-                                    :disabled="isstarted || seconds<1"
-                                    v-on:click="start"
-                            >
-                                {{btnstart}}
-                                <i v-if="!isstarted" class="fa fa-play-circle"></i>
-                                <span v-if="isstarted" class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
-                            </button>
-                        </div>
-
-                        <div v-if="!isstarted && (hh>0 || mm>0 || ss>0)" class="col-4 col-sm-2 col-md-2 col-lg-2 col-xl-1">
+                        <div v-if="(v>0 || i>0 || r>0)" class="col-4 col-sm-2 col-md-2 col-lg-2 col-xl-1">
                             <button type="button" class="btn btn-danger mt-4"
                                     v-on:click="clear"
                             >
@@ -86,5 +67,5 @@
             </div>
     </div>
     @endverbatim
-    <script type="module" src="/js/open/service/vue-ohmslaw.js"></script>
+    <script type="module" src="/js/open/service/electronic/vue-ohmslaw.js"></script>
 @endsection
