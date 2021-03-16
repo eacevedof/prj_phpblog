e672f73b0dc099697f120633fa4e18e4f44195b0
ioedumac <eacevedof@yahoo.es>
2021-03-16 23:06:21 +0100

    canonical en todo

--- backend_web/app/Http/Controllers/Open/ContactController.php
+++ backend_web/app/Http/Controllers/Open/ContactController.php
@@ -7,15 +7,17 @@ use App\Services\Restrict\Post\PostIndexService;
 
 class ContactController extends BaseController
 {
-    /**
-     * @return View
-     */
+
     public function __invoke()
     {
+        $breadscrumb = $this->_get_scrumb("open.home.contact");
+        $canonical = $this->_get_canonical($breadscrumb);
+
         return view('open.home.contact', [
             "result"      => [],
             "seo"         => SeoComponent::get_meta("open.home.contact"),
-            "breadscrumb" => $this->_get_scrumb("open.home.contact"),
+            "canonical"   => $canonical,
+            "breadscrumb" => $breadscrumb,
             "submenublog" => $this->_get_submenu_blog(),
             "submenuservice" => $this->_get_submenu_service(),
             "catslug"     => "contact",
